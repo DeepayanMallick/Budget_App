12992d015cd78b10de13f99b61be308a0c054b4e
Deepayan Mallick <deepayan1@live.com>
2018-11-12 15:52:29 +0600

    the project is not done yet

--- budget/views.py
+++ budget/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Project
 
 def project_list(request):
     return render(request, 'budget/product_list.html', {})
 
 def project_detail(request, project_slug):
-    return render(request, 'budget/project_detail.html', {})
+    project = get_object_or_404(Project, slug=project_slug)
+    return render(request, 'budget/project_detail.html', {'project': project})
